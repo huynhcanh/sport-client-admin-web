6e2d35e61c260c1a4422dc30986ce078794e0a3d
huynhcanh <huynhngoccanh3010@gmail.com>
2022-06-21 17:28:34 +0700

    .

--- src/main/java/com/laptrinhjavaweb/constant/SystemConstant.java
+++ src/main/java/com/laptrinhjavaweb/constant/SystemConstant.java
@@ -1,5 +1,5 @@
 package com.laptrinhjavaweb.constant;
 
 public class SystemConstant {
-    public static final String BASE_API = "http://17e3-180-93-146-1.ngrok.io";//"http://localhost:8081";
+    public static final String BASE_API = "https://6a73-27-78-71-221.ap.ngrok.io";//"http://localhost:8081";
 }
